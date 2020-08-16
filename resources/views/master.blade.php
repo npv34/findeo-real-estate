8d8bb9d508550f876e0b772081a2a6deb15547d1
npv34 <phanvanluan611996@gmail.com>
2020-08-16 20:00:47 +0700

    font-end view home page

---
+++ resources/views/master.blade.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<head>
+
+    <!-- Basic Page Needs
+    ================================================== -->
+    <title>Findeo</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/color.css') }}">
+
+</head>
+
+<body>
+
+<!-- Wrapper -->
+<div id="wrapper">
+
+
+    <!-- Compare Properties Widget
+    ================================================== -->
+    <div class="compare-slide-menu">
+
+        <div class="csm-trigger"></div>
+
+        <div class="csm-content">
+            <h4>Compare Properties <div class="csm-mobile-trigger"></div></h4>
+
+            <div class="csm-properties">
+
+                <!-- Property -->
+                <div class="listing-item compact">
+                    <a href="single-property-page-2.html" class="listing-img-container">
+                        <div class="remove-from-compare"><i class="fa fa-close"></i></div>
+                        <div class="listing-badges">
+                            <span>For Sale</span>
+                        </div>
+                        <div class="listing-img-content">
+                            <span class="listing-compact-title">Eagle Apartments <i>$420,000</i></span>
+                        </div>
+                        <img src="images/listing-01.jpg" alt="">
+                    </a>
+                </div>
+
+                <!-- Property -->
+                <div class="listing-item compact">
+                    <a href="single-property-page-2.html" class="listing-img-container">
+                        <div class="remove-from-compare"><i class="fa fa-close"></i></div>
+                        <div class="listing-badges">
+                            <span>For Sale</span>
+                        </div>
+                        <div class="listing-img-content">
+                            <span class="listing-compact-title">Selway Apartments <i>$420,000</i></span>
+                        </div>
+                        <img src="images/listing-03.jpg" alt="">
+                    </a>
+                </div>
+
+                <!-- Property -->
+                <div class="listing-item compact">
+                    <a href="single-property-page-2.html" class="listing-img-container">
+                        <div class="remove-from-compare"><i class="fa fa-close"></i></div>
+                        <div class="listing-badges">
+                            <span>For Sale</span>
+                        </div>
+                        <div class="listing-img-content">
+                            <span class="listing-compact-title">Oak Tree Villas <i>$535,000</i></span>
+                        </div>
+                        <img src="images/listing-05.jpg" alt="">
+                    </a>
+                </div>
+
+            </div>
+
+            <div class="csm-buttons">
+                <a href="compare-properties.html" class="button">Compare</a>
+                <a href="#" class="button reset">Reset</a>
+            </div>
+        </div>
+
+    </div>
+    <!-- Compare Properties Widget / End -->
+
+
+    <!-- Header Container
+    ================================================== -->
+    @include('layout.core.header')
+    <div class="clearfix"></div>
+    <!-- Header Container / End -->
+
+
+    @yield('content')
+    <!-- Footer
+    ================================================== -->
+    @include('layout.core.footer')
+    <!-- Footer / End -->
+
+
+    <!-- Back To Top Button -->
+    <div id="backtotop"><a href="#"></a></div>
+
+
+    <!-- Scripts
+    ================================================== -->
+    <script type="text/javascript" src="{{ asset('scripts/jquery-3.4.1.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('scripts/jquery-migrate-3.1.0.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('scripts/chosen.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('scripts/magnific-popup.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('scripts/owl.carousel.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('scripts/rangeSlider.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('scripts/sticky-kit.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('scripts/slick.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('scripts/masonry.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('scripts/mmenu.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('scripts/tooltips.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('scripts/custom.js') }}"></script>
+
+    <!-- Google Autocomplete -->
+    <script>
+        function initAutocomplete() {
+            var input = document.getElementById('autocomplete-input');
+            var autocomplete = new google.maps.places.Autocomplete(input);
+
+            autocomplete.addListener('place_changed', function() {
+                var place = autocomplete.getPlace();
+                if (!place.geometry) {
+                    window.alert("No details available for input: '" + place.name + "'");
+                    return;
+                }
+            });
+        }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?libraries=places&callback=initAutocomplete"></script>
+</div>
+<!-- Wrapper / End -->
+
+
+</body>
+</html>
